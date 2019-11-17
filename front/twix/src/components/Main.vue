410e9cdb894ddb020a0d94744dd69d348a7e6bb9
MarinaOct <marinaoctober98@gmail.com>
2019-11-17 07:43:27 +0300

    add loader

--- front/twix/src/components/Main.vue
+++ front/twix/src/components/Main.vue
@@ -8,6 +8,8 @@
               <input id="file" type="file" @change="onFileChange" style='width: 105px;'/>
               </label>
             </b-card-text>
+            
+            <div v-if="loader" class="lds-heart"><div></div></div>
             <b-card-img v-if="url" :src="url"></b-card-img>
             <b-card-text>
                  <b-table striped hover :items="response"></b-table>
@@ -49,6 +51,7 @@ export default {
   data() {
     return {
       url: null,
+      loader: false,
       response: [
           {'name': 'Волосеные трещены', 'accuracy': 0.95},
           {'name': 'Волосеные трещены', 'accuracy': 0.93},
@@ -105,13 +108,17 @@ export default {
           'Content-Type' : 'multipart/form-data'
         }
       };  
-
+      var app = this;
+      app.loader = true;
       axios.post(api+'predict/predict/', data, config).then(response => {
         // eslint-disable-next-line no-console
         console.log('response', response)
+        app.loader = false;
       }).catch(error => {
         // eslint-disable-next-line no-console
         console.log('error', error)
+        alert("Что-то пошло не так :с")
+        app.loader = false;
       })
 
 
@@ -159,4 +166,63 @@ body {
   max-width: 100%;
   max-height: 500px;
 }
+
+
+
+
+.lds-heart {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  transform: rotate(45deg);
+  transform-origin: 40px 40px;
+}
+.lds-heart div {
+  top: 32px;
+  left: 32px;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  background: #ff5050;
+  animation: lds-heart 1.2s infinite cubic-bezier(0.215, 0.61, 0.355, 1);
+}
+.lds-heart div:after,
+.lds-heart div:before {
+  content: " ";
+  position: absolute;
+  display: block;
+  width: 32px;
+  height: 32px;
+  background: #ff5050;
+}
+.lds-heart div:before {
+  left: -24px;
+  border-radius: 50% 0 0 50%;
+}
+.lds-heart div:after {
+  top: -24px;
+  border-radius: 50% 50% 0 0;
+}
+@keyframes lds-heart {
+  0% {
+    transform: scale(0.95);
+  }
+  5% {
+    transform: scale(1.1);
+  }
+  39% {
+    transform: scale(0.85);
+  }
+  45% {
+    transform: scale(1);
+  }
+  60% {
+    transform: scale(0.95);
+  }
+  100% {
+    transform: scale(0.9);
+  }
+}
+
 </style>
